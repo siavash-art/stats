cc5e15046baea90fcb40e1745efc7679ee5b3f87
Siyovush <siyovush@go.alif.academy>
2020-09-28 08:37:07 +0500

    add function CategoriesTotal

--- pkg/stats/stats_test.go
+++ pkg/stats/stats_test.go
@@ -68,6 +68,29 @@ func TestFilterByCategory_foundMultiple(t *testing.T) {
 
 	if !reflect.DeepEqual(expected, result) {
 		t.Errorf("invalid result, expected: %v, actual: %v", expected, result)
+	}	
+}
+
+func TestFilterByCategories_Total(t *testing.T) {
+
+	payments := []types.Payment {
+		{ID: 1, Category: "auto", Amount: 1_000_00},
+		{ID: 2, Category: "food", Amount: 2_000_00},
+		{ID: 3, Category: "auto", Amount: 3_000_00},
+		{ID: 4, Category: "auto", Amount: 4_000_00},
+		{ID: 5, Category: "fun", Amount: 5_000_00},
+	}
+	
+	expected := map[types.Category]types.Money {
+		"auto": 8_000_00,
+		"food": 2_000_00,
+		"fun": 5_000_00,
+	}
+
+	result := CategoriesTotal(payments)
+
+	if !reflect.DeepEqual(expected, result) {
+		t.Errorf("invalid result, expected: %v, actual: %v", expected, result)
 	}
 	
 }
